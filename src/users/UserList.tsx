57fec35bf1bd9a6ddb13b0b9a165878ffe8b2758
fzaninotto <fzaninotto@gmail.com>
2024-10-08 15:54:06 +0200

    initial commit

---
+++ src/users/UserList.tsx
@@ -0,0 +1,74 @@
+/* eslint react/jsx-key: off */
+import PeopleIcon from '@mui/icons-material/People';
+import memoize from 'lodash/memoize';
+import { useMediaQuery, Theme } from '@mui/material';
+import * as React from 'react';
+import {
+    BulkDeleteWithConfirmButton,
+    Datagrid,
+    List,
+    SearchInput,
+    SimpleList,
+    TextField,
+    TextInput,
+    usePermissions,
+} from 'react-admin';
+
+import Aside from './Aside';
+import UserEditEmbedded from './UserEditEmbedded';
+export const UserIcon = PeopleIcon;
+
+const getUserFilters = (permissions: string): React.ReactElement[] => {
+    const filters = [
+        <SearchInput source="q" alwaysOn />,
+        <TextInput source="name" />,
+    ];
+    if (permissions === 'admin') {
+        filters.push(<TextInput source="role" />);
+    }
+    return filters;
+};
+
+const UserBulkActionButtons = props => (
+    <BulkDeleteWithConfirmButton {...props} />
+);
+
+const rowClick = memoize(permissions => () => {
+    return permissions === 'admin'
+        ? Promise.resolve('edit')
+        : Promise.resolve('show');
+});
+
+const UserList = () => {
+    const { permissions } = usePermissions();
+    return (
+        <List
+            filters={getUserFilters(permissions)}
+            filterDefaultValues={{ role: 'user' }}
+            sort={{ field: 'name', order: 'ASC' }}
+            aside={<Aside />}
+        >
+            {useMediaQuery((theme: Theme) => theme.breakpoints.down('md')) ? (
+                <SimpleList
+                    primaryText={record => record.name}
+                    secondaryText={record =>
+                        permissions === 'admin' ? record.role : null
+                    }
+                />
+            ) : (
+                <Datagrid
+                    rowClick={rowClick(permissions)}
+                    expand={<UserEditEmbedded />}
+                    bulkActionButtons={<UserBulkActionButtons />}
+                    optimized
+                >
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    {permissions === 'admin' && <TextField source="role" />}
+                </Datagrid>
+            )}
+        </List>
+    );
+};
+
+export default UserList;
